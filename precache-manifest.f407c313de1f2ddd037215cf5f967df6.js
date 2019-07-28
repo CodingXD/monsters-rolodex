33fe548779828917ddba374179c39084ca08e938
Marshall Shelly <Marshallshelly97@gmail.com>
2019-07-28 14:50:37 +0530

    Updates

---
+++ precache-manifest.f407c313de1f2ddd037215cf5f967df6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248be73abd7838077c9192857644897e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ffe761ce7f985fda94e0",
+    "url": "/monsters-rolodex/static/css/main.ae383807.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "ffe761ce7f985fda94e0",
+    "url": "/monsters-rolodex/static/js/main.967044f4.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
